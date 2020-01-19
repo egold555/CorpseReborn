087e384febe94d7e2074dedfc95c886f0a3069c1
Eric Golde <eric@golde.org>
2020-01-18 17:21:22 -0800

    been so long idk what has changed

--- src/org/golde/bukkit/corpsereborn/Lang.java
+++ src/org/golde/bukkit/corpsereborn/Lang.java
@@ -1,123 +1,139 @@
 package org.golde.bukkit.corpsereborn;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.Level;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.golde.bukkit.corpsereborn.dump.ReportError;
 
 public class Lang {
 
-	private static YamlConfiguration LANG_YML;
-	private static File LANG_FILE;
-	
-	public enum Msgs {
-	    NO_PERMS("no-permissions", "&cYou don''t have permission for that!");
-	 
-	    private String path;
-	    private String def;
-	    private static YamlConfiguration LANG;
-	 
-	    /**
-	    * Lang enum constructor.
-	    * @param path The string path.
-	    * @param start The default string.
-	    */
-	    Msgs(String path, String start) {
-	        this.path = path;
-	        this.def = start;
-	    }
-	 
-	    /**
-	    * Set the {@code YamlConfiguration} to use.
-	    * @param config The config to set.
-	    */
-	    public static void setFile(YamlConfiguration config) {
-	        LANG = config;
-	    }
-	 
-	    @Override
-	    public String toString() {
-	        return ChatColor.translateAlternateColorCodes('&', LANG.getString(this.path, def));
-	    }
-	 
-	    /**
-	    * Get the default value of the path.
-	    * @return The default value of the path.
-	    */
-	    public String getDefault() {
-	        return this.def;
-	    }
-	 
-	    /**
-	    * Get the path to the string.
-	    * @return The path to the string.
-	    */
-	    public String getPath() {
-	        return this.path;
-	    }
+	private static String guiName;
+	private static String username;
+	private static List<String> finishLootingMessage = new ArrayList<String>();
+
+	public static String getInventoryName(Player p){return guiName.replaceAll("%corpse%", p.getName()).replaceAll("&", "§");}
+
+	@Deprecated
+	public static String getUsername(Player p, String overrideUsername){
+		return getUsername(p.getName(), overrideUsername);
 	}
-	
-	/**
-	 * Load the lang.yml file.
-	 * @return The lang.yml config.
-	 */
-	public boolean loadLang() {
-	    File lang = new File(Main.getPlugin().getDataFolder(), "lang.yml");
-	    if (!lang.exists()) {
-	        try {
-	        	Main.getPlugin().getDataFolder().mkdir();
-	            lang.createNewFile();
-	            InputStream defConfigStream = Main.getPlugin().getResource("lang.yml");
-	            if (defConfigStream != null) {
-	                YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
-	                defConfig.save(lang);
-	                Msgs.setFile(defConfig);
-	            }
-	        } 
-	        catch(IOException e) {
-	            new ReportError(e); // So they notice
-	            Util.cinfo("&cCouldn't create language file.");
-	            Util.cinfo("&cThis is a fatal error. Now disabling");
-	            return false; // Without it loaded, we can't send them messages
-	        }
-	    }
-	    YamlConfiguration conf = YamlConfiguration.loadConfiguration(lang);
-	    for(Msgs item:Msgs.values()) {
-	        if (conf.getString(item.getPath()) == null) {
-	            conf.set(item.getPath(), item.getDefault());
-	        }
-	    }
-	    Msgs.setFile(conf);
-	    Lang.LANG_YML = conf;
-	    Lang.LANG_FILE = lang;
-	    try {
-	        conf.save(getLangFile());
-	    } catch(IOException e) {
-	    	Util.cinfo("&cFailed to save lang.yml.");
-	        new ReportError(e);
-	        return false;
-	    }
-		return true;
+
+	public static String getUsername(String pUsername, String overrideUsername){
+		if(overrideUsername == null){
+			overrideUsername = username.replaceAll("%corpse%", pUsername).replaceAll("&", "§");
+		}
+
+		if(overrideUsername.length() > 16) {
+			overrideUsername = overrideUsername.substring(0, 16);
+		}
+		return overrideUsername;
 	}
-	
-	/**
-	* Gets the lang.yml config.
-	* @return The lang.yml config.
-	*/
-	public YamlConfiguration getLang() {
-	    return LANG_YML;
+	public static List<String> finishLootingMessage(String name){
+		if(finishLootingMessage.size() == 0) {
+			return null;
+		}
+
+		if(finishLootingMessage.get(0).equalsIgnoreCase("none")){
+			return null;
+		}
+
+		List<String> toReturn = new ArrayList<String>();
+
+		for(String repl : finishLootingMessage) {
+			toReturn.add(repl.replaceAll("%corpse%", name).replaceAll("&", "§"));
+		}
+
+		return toReturn;
 	}
-	 
-	/**
-	* Get the lang.yml file.
-	* @return The lang.yml file.
-	*/
-	public File getLangFile() {
-	    return LANG_FILE;
+
+	public static void checkConfigForMissingOptions()
+	{
+		FileConfiguration config = Main.getPlugin().getConfig();
+
+		if (! config.isSet("gui-title")) {
+			appendConfig("#Title of inventory created when you click the corpses head to loot it. ",
+					"#%corpse% gets replaced with the corpses name.",
+					"#Color codes and unicode characters work, but might be glitchy.",
+					"#Minecraft has a 32 character limit on GUI lengths. If your title is more then that you will get a error on the console.",
+					"gui-title: \"%corpse%'s Items\"");
+		}
+
+		if (! config.isSet("username-format")) {
+			appendConfig("#This is the username of the corpse.",
+					"#%corpse% gets replaced with the corpses name.",
+					"#Color codes and unicode characters work, but might be glitchy.",
+					"#Minecraft has a 16 character limit on name lengths. If your title is more then that you will get a error on the console.",
+					"username-format: \"%corpse%\"");
+		}
+
+		if (! config.isSet("finish-looting-message")) {
+			appendConfig("#This is the message sent when you finish looting the corpse. Set the message to \"none\" to disable.",
+					"#%corpse% gets replaced with the corpses name.",
+					"#Color codes and unicode characters work, but might be glitchy.",
+					"finish-looting-message: \"&bYou have finished looting %corpse%'s corpse.\"");
+		}
+
 	}
-	
+
+	private static String newLine = "\r\n";
+
+	private static void appendConfig(String... lines)
+	{
+		File dataDir = Main.getPlugin().getDataFolder();
+		if (!dataDir.exists())
+			dataDir.mkdirs();
+
+		File file = new File(dataDir, "config.yml");
+
+		try {
+			FileWriter writer = new FileWriter(file, true);
+			writer.append(newLine);
+			for (String line: lines) {
+
+				writer.append(line);
+				writer.append(newLine);
+			}
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void load() {
+		try {
+			FileConfiguration config = Main.getPlugin().getConfig();
+
+			guiName = config.getString("gui-title", "%corpse%'s Items");
+			username = config.getString("username-format", "%corpse%");
+			String finishLootingMessageString = config.getString("finish-looting-message", "&bYou have finished looting %corpse%'s corpse.");
+
+			//work around for upgrading configs. finish-looting-message can now also support mutiple messages
+
+			if(finishLootingMessageString.charAt(0) == '[' && finishLootingMessageString.charAt(finishLootingMessageString.length() - 1) == ']') {
+				finishLootingMessage = config.getStringList("finish-looting-message");
+			}
+			else {
+				finishLootingMessage.add(finishLootingMessageString);
+			}
+			
+			Util.info("Config successfully loaded.");
+
+		} catch (Exception e) {
+			Util.severe("================================");
+			Util.severe("Could not load language file!");
+			Util.severe("Is it configured properly?");
+			Util.severe("Have you deleted old configs?");
+			Util.severe("================================");
+			new ReportError(e);
+			Main.getPlugin().cont = false;
+			Bukkit.getServer().getPluginManager().disablePlugin(Main.getPlugin());
+		}
+	}
+
 }
